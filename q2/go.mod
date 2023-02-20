b419899c44239972b1f95af0881273df1b08849d
JWEmmanuel89 <2005112848.ub.edu.com>
2023-02-19 21:06:48 -0600

    q2 channels

---
+++ q2/go.mod
@@ -0,0 +1,3 @@
+module github.com/JWEmmanuel89/Quiz-2/q2
+
+go 1.19
---
+++ q2/main.go
@@ -0,0 +1,35 @@
+// Project: Go channels (Buffered Channel)
+package main
+
+import "fmt"
+
+func main() {
+	//Create variables and channels
+	location := make(chan string, 3)
+	duration := make(chan string, 3)
+	size := make(chan int64, 3)
+	vesselName := make(chan string, 3)
+
+	//Input data into channels
+	location <- "North"
+	location <- "Central"
+	location <- "South"
+	duration <- "6 weeks"
+	duration <- "4 weeks"
+	duration <- "8 weeks"
+	size <- 12
+	size <- 8
+	size <- 10
+	vesselName <- "Vessel 1"
+	vesselName <- "Vessel 5"
+	vesselName <- "Vessel 10"
+
+	//Print data
+	for i := 0; i < 3; i++ {
+		fmt.Println("AOR:", <-location)
+		fmt.Println("Duration:", <-duration)
+		fmt.Println("Team Size:", <-size)
+		fmt.Println("Vessel Name:", <-vesselName)
+		fmt.Println()
+	}
+}
